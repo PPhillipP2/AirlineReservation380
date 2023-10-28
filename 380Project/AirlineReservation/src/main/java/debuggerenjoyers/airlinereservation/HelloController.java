0ebe9f8d463f73468b66bbd936ca3ac7cefa26be
AngelMerchant <angel.merchant.192@my.csun.edu>
2023-10-27 17:32:16 -0700

    Colored UI

--- 380Project/AirlineReservation/src/main/java/debuggerenjoyers/airlinereservation/HelloController.java
+++ 380Project/AirlineReservation/src/main/java/debuggerenjoyers/airlinereservation/HelloController.java
@@ -1,14 +1,44 @@
 package debuggerenjoyers.airlinereservation;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private RadioButton oneWayRadioButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private RadioButton roundTripRadioButton;
+
+    @FXML
+    private Button searchFlightsButton;
+
+    @FXML
+    private DatePicker departureDatePicker;
+
+    @FXML
+    private DatePicker arrivalDatePicker;
+
+    @FXML
+    private TextField passengersTextField;
+
+    @FXML
+    private ChoiceBox<String> originChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> destinationChoiceBox;
+
+    @FXML
+    private CheckBox resultCheckBox1;
+
+    @FXML
+    private CheckBox resultCheckBox2;
+
+    @FXML
+    private CheckBox resultCheckBox3;
+
+    @FXML
+    private Accordion accordion;
+
+    // You can add more fields as needed to match other elements in your FXML file.
 }
