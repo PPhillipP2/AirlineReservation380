9cb89498798ab57966ec2201061a8cd933183871
AngelMerchant <angel.merchant.192@my.csun.edu>
2023-11-01 14:59:21 -0700

    List View method (btw changed departDate to string from date, to test the list)

--- 380Project/AirlineReservation/src/main/java/debuggerenjoyers/airlinereservation/Flight.java
+++ 380Project/AirlineReservation/src/main/java/debuggerenjoyers/airlinereservation/Flight.java
@@ -4,22 +4,24 @@ import java.util.Date;
 
 public class Flight {
 
-    private int flightID;
-    private String departAirport;
-    private String arrivalAirport;
-    private Date departDate;
-    private String departTime;
-    private int seatsOpen;
-    private String seatChart;
-    private double price;
+    private final int flightID;
+    private final String departAirport;
+    private final String arrivalAirport;
+    private final String departDate;
+    private final String departTime;
+    private final String arrivalTime;
+    private final int seatsOpen;
+    private final String seatChart;
+    private final double price;
 
-    public Flight(int flightID, String departAirport, String arrivalAirport, Date departDate,
-                  String departTime, int seatsOpen, String seatChart, double price) {
+    public Flight(int flightID, String departAirport, String arrivalAirport, String departDate,
+                  String departTime, String arrivalTime, int seatsOpen, String seatChart, double price) {
         this.flightID = flightID;
         this.departAirport = departAirport;
         this.arrivalAirport = arrivalAirport;
         this.departDate = departDate;
         this.departTime = departTime;
+        this.arrivalTime = arrivalTime;
         this.seatsOpen = seatsOpen;
         this.seatChart = seatChart;
         this.price = price;
@@ -38,12 +40,16 @@ public class Flight {
         return arrivalAirport;
     }
 
-    public Date getDepartDate() {
+    public String getDepartDate() {
         return departDate;
     }
 
     public String getDepartTime() {
         return departTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
     }
 
     public int getSeatsOpen() {
