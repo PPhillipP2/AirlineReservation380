1f4906f38b280c19d5635f5d4f482b04c9d092ff
EdProgram <daketinx@gmail.com>
2023-11-22 15:33:46 -0800

    ConfirmationUI and ConfirmationController

---
+++ 380Project/AirlineReservation/src/main/java/debuggerenjoyers/airlinereservation/ConfirmationController.java
@@ -0,0 +1,46 @@
+package debuggerenjoyers.airlinereservation;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+
+
+
+public class ConfirmationController {
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private Text textElement; // This corresponds to the first Text element
+
+    @FXML
+    private Text confirmationText; // This corresponds to the second Text element
+
+    @FXML
+    private Button returnButton;
+
+    // You can add an initialize method if needed
+    @FXML
+    private void initialize() {
+        // You can perform any initialization here
+    }
+
+    // You can add event handlers for the button or any other controls as needed
+    @FXML
+    private void handleReturnButtonAction() {
+        // Code to handle the return button click event
+    }
+
+    // Other methods or variables as needed
+}
+
+
+
+
+
+
